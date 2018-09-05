83b80121036b3ef74028391a4b7633da9609cdb7
Brent Postlethweight <brentpostle@gmail.com>
2018-09-05 01:54:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  recipes[key] = value
-  return recipes;
+  return object[key] = value;
 }
 
 function deleteFromObjectByKey(object, key) {

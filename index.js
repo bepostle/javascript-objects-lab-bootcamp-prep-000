cd2276cdc0a334b003b3af3b0d0af10b7400e8cb
Brent Postlethweight <brentpostle@gmail.com>
2018-09-05 01:35:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign(key, value);
+  return recipes;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
+

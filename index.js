b6149e2f3ba88ff38e9c2d45f88511f335b8f8ef
Brent Postlethweight <brentpostle@gmail.com>
2018-09-05 02:13:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  return Object.delete[key];
+  new Object
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {

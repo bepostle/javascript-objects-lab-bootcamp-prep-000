3d20dd1e748c947259eea55000981c1545d974c8
Brent Postlethweight <brentpostle@gmail.com>
2018-09-05 01:43:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {butter: '1 cup'};
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  return Object.assign({recipes}, {key: value})
+  return Object.assign(recipes, {key: value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
